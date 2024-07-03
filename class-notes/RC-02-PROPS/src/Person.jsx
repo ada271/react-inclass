8644c1d07d0031f0d91c4f8daccc49b654c5bd73
ada <h.kaansolmaz@gmail.com>
2024-07-03 13:38:35 +0300

    props-1

--- class-notes/RC-02-PROPS/src/Person.jsx
+++ class-notes/RC-02-PROPS/src/Person.jsx
@@ -1,8 +1,19 @@
 
 
-const Person = () => {
+const Person = (props) => {
+
+    console.log(props)
+
+    //dest
+    const{name, img, tel}=props;
   return (
-    <div>Person</div>
+    <div>
+        <h1>
+            {name}            
+        </h1>
+        <img src={img} alt="" />
+        <p>{tel}</p>
+    </div>
   )
 }
 
