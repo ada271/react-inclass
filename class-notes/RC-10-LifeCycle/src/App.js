c425c7cfd73a611c8336d668712c79965002ecd8
ada <h.kaansolmaz@gmail.com>
2024-07-18 10:42:15 +0300

    ff

--- class-notes/RC-10-LifeCycle/src/App.js
+++ class-notes/RC-10-LifeCycle/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import LifeCycleMethods from "./LifeCycleMethods";
 
 function App() {
   return (
-    <div className="App">
-      <h1>DE09</h1>
+    <div>
+    
+    <LifeCycleMethods/>
     </div>
   );
 }
